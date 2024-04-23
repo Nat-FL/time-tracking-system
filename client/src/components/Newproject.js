e0d27bc32451047c7db35f6eeb3c508f2645dcda
ColinClark383 <colin.clark@ufl.edu>
2024-04-23 16:58:21 -0400

    added project creation page

---
+++ client/src/components/Newproject.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+
+
+
+const Newproject = () => {
+    const navigate = useNavigate();
+    const [name, setName] = useState('');
+    const [start, setStart] = useState('');
+    const [end, setEnd] = useState('');
+    const [Employee, setEmployee] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('/createProject', { name, start, end, Employee });
+            console.log(response.data); 
+            navigate("/projects");
+
+        } catch (error) {
+            setError('Invalid Input!');
+        }
+    };
+
+    return (
+        <div>
+            <h2>Add a Project</h2>
+            <form onSubmit={handleSubmit}>
+                <div style={{ marginBottom: '16px' }}>                <input type="text" placeholder="Project Name" value={name} onChange={(e) => setName(e.target.value)} required />
+                </div>
+                <div style={{ marginBottom: '16px' }}>                <input type="time" placeholder="Start Time" value={start} onChange={(e) => setStart(e.target.value)} required />
+                </div>
+                <div style={{ marginBottom: '16px' }}>                <input type="time" placeholder="End Time" value={end} onChange={(e) => setEnd(e.target.value)} required />
+                </div>
+                
+                <div style={{ marginBottom: '16px' }}>
+                <label>Users:</label>
+                <input type="text" placeholder="employees" value={Employee} onChange={(e) => setEmployee(e.target.value)} required />
+                </div>
+
+                <div style={{ marginBottom: '16px' }}>                <button type="submit">Create Project</button>
+                </div>
+            </form>
+            {error && <p>{error}</p>}
+        </div>
+    );
+};
+
+export default Newproject;
--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -2,10 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Projects = () => {
+
+  //variable needs to work on backend
+  var isAdmin = true
+
   return (
     <div  style={{ backgroundColor: 'white', color: 'black', padding: "80px 60px", minHeight: '100vh-100px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
       <h1>Project 1 - Funding</h1>
       <Link to="/data">Click here for more information.</Link>
+      {isAdmin && (
+        <Link to='/newproject'>
+          <button class="button-28" role="button" style={{width: '250px'}}>Create an New Project</button>
+        </Link>
+      )}
     </div>
   );
 }
