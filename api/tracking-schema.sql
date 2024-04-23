75701545aae3af2558883605cb0708b4828468eb
Nat-FL <nrgraddy@gmail.com>
2024-04-23 14:20:43 -0400

    updated schema and created user model

---
+++ api/tracking-schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE users(
+    user_id             SERIAL PRIMARY KEY, 
+    name                TEXT, 
+    password            TEXT, 
+    status              TEXT, 
+    associated_clients  TEXT 
+); 
+
+CREATE TABLE projects(
+    project_id     SERIAL PRIMARY KEY, 
+    project_name   TEXT NOT NULL, 
+    user_ID    INTEGER NOT NULL 
+); 
+
+CREATE TABLE projectIsON(
+    projectOn_id   SERIAL PRIMARY KEY, 
+    user_ID        INTEGER NOT NULL, 
+    projectId      INTEGER NOT NULL, 
+    FOREIGN KEY (user_ID) REFERENCES users(user_id),
+    FOREIGN KEY (projectId) REFERENCES projects(project_id)
+);
+
+CREATE TABLE timesheet (
+    timesheet_id  SERIAL PRIMARY KEY, 
+    user_ID    INTEGER NOT NULL,
+    time_record TIMESTAMP DEFAULT NOW(), 
+    FOREIGN KEY (user_ID) REFERENCES users(user_id)
+);
